1d18b91e57d9c9e8d790e62474da585bb886fd31
Ramon Byfield <ramon.byfield@hotmail.com>
2025-11-14 20:24:13 -0500

    lets wrap it up

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('#board div');
     const status = document.getElementById('status');
+    const newGameBtn = document.querySelector('.btn');
+
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ];
 
     squares.forEach(square => {
         square.classList.add('square');
         square.dataset.turn = 'X';
 
         square.addEventListener('click', () => {
+            if (square.textContent) return; // if square has stuff do nothing
+
             const current = square.dataset.turn;
             square.textContent = current;
             square.classList.remove('X', 'O');
             square.classList.add(current);
             square.dataset.turn = current === 'X' ? 'O' : 'X';
+            checkWinner();
         });
 
         square.addEventListener('mouseenter', () => {
@@ -21,5 +31,30 @@ document.addEventListener('DOMContentLoaded', () => {
         square.addEventListener('mouseleave', () => {
             square.classList.remove('hover');
         });
+    });
+
+    function checkWinner() { //iterates through winning combinations n checks those squares for a match
+        for (let combo of winningCombos) {
+            const [a, b, c] = combo;
+            const markA = squares[a].textContent;
+            const markB = squares[b].textContent;
+            const markC = squares[c].textContent;
+
+            if (markA && markA === markB && markA === markC) {
+                status.textContent = `Congratulations! ${markA} is the Winner!`;
+                status.classList.add('you-won');
+                break;
+            }
+        }
+    }
+
+    newGameBtn.addEventListener('click', () => { // sets slate clean
+        squares.forEach(square => {
+            square.textContent = '';
+            square.classList.remove('X', 'O');
+            square.dataset.turn = 'X';
+        });
+        status.textContent = 'Move your mouse over a square and click to play an X or an O.';
+        status.classList.remove('you-won');
     });
 });
